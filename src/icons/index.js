0970a0025ac93efc812e64307daf8539e4a24de8
arling-github <769806511@qq.com>
2022-10-08 10:09:44 +0800

    人力资源项目初始化

---
+++ src/icons/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import SvgIcon from '@/components/SvgIcon'// svg component
+
+// register globally
+Vue.component('svg-icon', SvgIcon)
+
+const req = require.context('./svg', false, /\.svg$/)
+const requireAll = requireContext => requireContext.keys().map(requireContext)
+requireAll(req)
+
+// 代码含义 ：将所有的svg目录下的图片引入到项目中
