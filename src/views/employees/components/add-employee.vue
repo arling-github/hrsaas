65e8a40a0d2f01dbc88a8bd59179580edb8b3855
arling-github <769806511@qq.com>
2022-12-29 22:52:08 +0800

    新增员工的弹出框补充

--- src/views/employees/components/add-employee.vue
+++ src/views/employees/components/add-employee.vue
@@ -2,7 +2,7 @@
   <!-- 共用弹出层 -->
   <el-dialog title="新增员工" :visible="showDialog" @close="showDialogoff">
     <!-- 表单 -->
-    <el-form label-width="120px" :model="formData" :rules="rules">
+    <el-form ref="addEmployee" label-width="120px" :model="formData" :rules="rules">
       <el-form-item label="姓名" prop="username">
         <el-input v-model="formData.username" style="width: 50%" placeholder="请输入姓名" />
       </el-form-item>
@@ -13,24 +13,30 @@
         <el-date-picker v-model="formData.timeOfEntry" style="width: 50%" placeholder="请选择入职时间" />
       </el-form-item>
       <el-form-item label="聘用形式" prop="formOfEmployment">
-        <el-select v-model="formData.formOfEmployment" style="width: 50%" placeholder="请选择" />
+        <el-select v-model="formData.formOfEmployment" style="width: 50%" placeholder="请选择">
+          <!-- 遍历只能遍历组件的数据 -->
+          <el-option v-for="item in EmployeeEnum.hireType" :key="item.id" :label="item.value" :value="item.id" />
+        </el-select>
       </el-form-item>
       <el-form-item label="工号" prop="workNumber">
         <el-input v-model="formData.workNumber" style="width: 50%" placeholder="请输入工号" />
       </el-form-item>
       <el-form-item label="部门" prop="departmentName">
-        <el-input v-model="formData.departmentName" style="width: 50%" placeholder="请选择部门" />
+        <el-input v-model="formData.departmentName" style="width: 50%" placeholder="请选择部门" @focus="getDepartments" />
+        <!-- 放一个树形组件 -->
+        <el-tree v-if="showTree" v-loading="loading" :data="treeData" :props="{ label: 'name' }" default-expand-all="" @node-click="selectNode" />
       </el-form-item>
       <el-form-item label="转正时间" prop="correctionTime">
-        <el-input v-model="formData.correctionTime" style="width: 50%" placeholder="请选择转正时间" />
+        <!-- <el-input v-model="formData.correctionTime" style="width: 50%" placeholder="请选择转正时间" /> -->
+        <el-date-picker v-model="formData.timeOfEntry" style="width: 50%" placeholder="请选择入职时间" />
       </el-form-item>
     </el-form>
     <!-- 表单确认按钮 -->
     <template v-slot:footer>
       <el-row type="flex" justify="center">
         <el-col :span="6">
-          <el-button size="samll" @click="showDialogoff">取消</el-button>
-          <el-button type="primary" size="samll">确定</el-button>
+          <el-button size="samll" @click="btnCancel">取消</el-button>
+          <el-button type="primary" size="samll" @click="btnOK">确定</el-button>
         </el-col>
       </el-row>
     </template>
@@ -38,6 +44,10 @@
 </template>
 
 <script>
+import { getDepartments } from '@/api/departments'
+import { addEmployee } from '@/api/employees'
+import { tranListToTreeData } from '@/utils'
+import EmployeeEnum from '@/api/constant/employees'
 export default {
   props: {
     showDialog: {
@@ -47,6 +57,11 @@ export default {
   },
   data() {
     return {
+      EmployeeEnum: EmployeeEnum,
+      // 表单数据
+      treeData: [],
+      showTree: false,
+      loading: false,
       // 表单校验数据结构
       formData: {
         username: '',
@@ -75,8 +90,54 @@ export default {
     }
   },
   methods: {
-    showDialogoff() {
-      this.showDialog = false
+    // showDialogoff() {
+    //   this.showDialog = false
+    // },
+    // 部门选择的时候树形结构数据
+    async getDepartments() {
+      this.showTree = true
+      this.loading = true
+      const { depts } = await getDepartments()
+      // depts是一个数组 需要转换成树形结构
+      this.treeData = tranListToTreeData(depts, '')
+      this.loading = false
+    },
+    // 点击时把内容填到输入框
+    selectNode(node) {
+      this.formData.departmentName = node.name
+      this.showTree = false
+    },
+    // 确定按钮
+    async btnOK() {
+      // 校验表单
+      try {
+        // 校验成功
+        await this.$refs.addEmployee.validate()
+        // 调用新增接口
+        await addEmployee(this.formData) // 新增员工
+        // 告诉父组件更新数据
+        // this.$parent 可以直接调用到父组件的实例 实际上就是父组件this 但是父组件不能放到element ui的组件里面
+        // this.$emit
+        this.$parent.getEmployeeList()
+        this.$parent.showDialog = false
+      } catch (error) {
+        // 失败
+        console.log(error)
+      }
+    },
+    btnCancel() {
+      // 重置原来的数据
+      this.formData = {
+        username: '',
+        mobile: '',
+        formOfEmployment: '',
+        workNumber: '',
+        departmentName: '',
+        timeOfEntry: '',
+        correctionTime: ''
+      }
+      this.$refs.addEmployee.resetFields() // 重置校验结果
+      this.$emit('update:showDialog', false)
     }
   }
 }
